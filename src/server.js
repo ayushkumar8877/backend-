8db1a66bd459beff5e2152b49d1357719afa9ce8
ayushkumar8877 <ayushkumar88770@gmail.com>
2025-07-15 15:17:00 +0530

    fix

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -15,14 +13,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// current directory fix for ES modules (since __dirname not available)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // CORS config
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // .env me set kar de URL
+    origin: process.env.CLIENT_URL, // .env me frontend ka URL daal de
     credentials: true,
   })
 );
@@ -36,16 +30,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/friends", friendRoutes);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-  app.use(express.static(frontendPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
-
+// Server start
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
   connectDB();
